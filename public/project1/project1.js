746f1f958f9779a9632314a98d0033d57336e843
Julian <jtiu.edu@gmail.com>
2019-10-23 12:12:44 -0500

    transferred from tamu github

---
+++ public/project1/project1.js
@@ -0,0 +1,122 @@
+var links;
+var buttons;
+var turn;
+var paint;
+var currentlyPainting;
+var instructions;
+
+window.onload = function(){
+    links = document.getElementsByClassName("link");
+    buttons = document.getElementsByClassName("b");
+    instructions = document.getElementsByClassName("instructions");
+    instructions[0].style.display = "block";
+    instructions[1].style.display = "none";
+    this.currentlyPainting = false;
+}
+
+function getCurrentAngle(link) {
+    // Getting the the css style set of a specific link from the html document
+    var styleSet = window.getComputedStyle(link);
+
+    // Getting the the css value of "transform" from the styleSet
+    var matrix = styleSet.getPropertyValue('transform');
+
+    /*
+        - Generating an array of values from matrix
+            - matrix holds the value "matrix(i, j, k, l)"
+            - We want it in the form of [i, j, k, l]
+        - To do this, we treat the value of matrix as a string and split accordingly
+            - split() creates an array that splits given a delimeter
+        - The first split results in ["matrix", "i,j,l)"]
+            - We only want to further split the values in the second index
+        - The second split results in ["i,j,k,l"]
+            - In this case, it will only be one index b/c there are no strings after ")"
+        - The last split has the char "," as the delimeter
+            - Resulting array is [i, j, k, l]
+    */
+    var splitting = matrix.split("(")[1];
+        splitting = splitting.split(")")[0];
+        splitting = splitting.split(",")
+
+    /*
+        To get the angle of the link, we need to calculate the value of arcsin(j)
+        and multiply it with (180/Pi) to get the resulting answer in degrees
+        instead of radians
+    */  
+    var angle = Math.round(Math.atan2(splitting[1],splitting[0]) * (180/Math.PI));
+
+    return angle;
+
+}
+
+function incrementCurrentAngle(link, currentAngle, increment) {
+    var deg = currentAngle + increment;
+    var newCSS = "\
+    -webkit-transform: rotate("+deg+"deg); \
+    -moz-transform: rotate("+deg+"deg); \
+    -ms-transform: rotate("+deg+"deg); \
+    -o-transform: rotate("+deg+"deg); \
+    transform: rotate("+deg+"deg); \
+    ";
+
+    link.style.cssText = newCSS;
+}
+
+function counterClockwise(n) {
+    turn=setInterval(function() {
+        var link = links[n];
+        var angle = getCurrentAngle(link);
+        incrementCurrentAngle(link, angle, -1);
+    }, 20);
+}
+
+function clockwise(n) {
+    turn=setInterval(function() {
+        var link = links[n];
+        var angle = getCurrentAngle(link);
+        incrementCurrentAngle(link, angle, 1);
+    }, 20);
+}
+
+function stopTurn() {
+    if (turn) clearInterval(turn);
+}
+
+function bloop() {
+    if(!currentlyPainting) {
+        currentlyPainting = true;
+        instructions[0].style.display = "none";
+        instructions[1].style.display = "block";
+        paint=setInterval(function() {
+            var dot = document.createElement("div");
+            dot.className = "circle";
+            dot.style.position = "absolute";
+        
+            var position = $(document.getElementById('tip')).offset();
+            var new_pos = $(dot).css(position)
+        
+            var x = new_pos.left;
+            var y = new_pos.top;
+        
+            dot.style.top = y;
+            dot.style.left = x;
+        
+            document.getElementById("window").appendChild(dot);
+        }, 10);
+    } else {
+        currentlyPainting = false;
+        instructions[0].style.display = "block";
+        instructions[1].style.display = "none";
+        clearInterval(paint);
+    }
+}
+
+/********************************************************************* RESOURCES
+    https://css-tricks.com/almanac/properties/t/transform-origin/
+    https://css-tricks.com/get-value-of-css-rotation-through-javascript/
+    https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+    https://stackoverflow.com/questions/15505272/javascript-while-mousedown
+    https://stackoverflow.com/questions/19574171/how-to-get-css-transform-rotation-value-in-degrees-with-javascript
+    https://www.useragentman.com/blog/2011/01/07/css3-matrix-transform-for-the-mathematically-challenged/
+    https://www.w3schools.com/jsref/event_onkeypress.asp
+*/
